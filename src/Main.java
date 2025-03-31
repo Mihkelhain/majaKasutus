c8c204828d92d53536376bd20995a29ada321225
Mihkel Hain <mihkelhain@ut.ee>
2025-04-01 02:40:41 +0300

    Alguse viimase osad

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); // teeb asju
+
+        veeKasutaja Series4 = new veeKasutaja("A","Series 4","Bosch",2.25,18.5);
+        koduMasin pliit = new koduMasin("C","GE pliit","General Electric",1.5);
+
+        System.out.println(pliit.elektriKasutus());
+        System.out.println(Series4.elektriKasutus());
+
+        System.out.println(pliit);
+        System.out.println(Series4);
+
+        //Peab tegema niimoodi, et see tegelikult küsib kasutaja käest mis asju ta tahaks lisada või vaadata.
     }
 
 }
